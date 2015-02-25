5d1c395516f8629345813b1038eba0c686b8cba0
David Moreno Lumbreras <dmorenolumb@gmail.com>
2015-02-25 12:56:22 +0100

    Optimizando...

--- webappmulti.py
+++ webappmulti.py
@@ -125,7 +125,7 @@ class sumaApp(app):
 class aleatApp(app):
 
 	def process(self, parsedRequest):
-		return ("200 OK", "<html><body><h1>ALEATORIO</h1><p><a href='http://localhost:1234/aleat/"+
+		return ("200 OK", "<html><body><h1>ALEATORIO</h1><p><a href='/aleat/"+
 				str(random.randrange(10000000000000)) +
 				"'>Dame Otra!!</a></p></body></html>")
 				
